407c2774270440f1b93f1d150225dbb5df2b552a
meiyaoxiang <756006942@qq.com>
2021-10-03 00:28:36 +0800

    revert: 修改评论组件引用路径的问题

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import Layout from './components/Layout.vue'
 import { ElIcon } from 'element-plus'
-import Comment from '@/components/comment/index.vue'
+import Comment from '@/components/comment/Index.vue'
 import './custom.css'
 import 'element-plus/theme-chalk/index.css'
 import 'tailwindcss/tailwind.css'
